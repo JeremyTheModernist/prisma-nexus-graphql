34eab39a36b07c2308268ed4935b401f71065e60
Jeremy Barnes <jeremy@wovenmagazine.com>
2021-03-23 10:55:01 -0400

    fix: removed the dist folder.

--- dist/api/context.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.context = void 0;
-const db_1 = require("./db");
-exports.context = {
-    db: db_1.db
-};
-//# sourceMappingURL=context.js.map
